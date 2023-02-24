75963febb859aed2fb8e46605515b747ac585405
Alexander Gorsc <alexandergorsc@gmail.com>
2023-02-24 11:39:58 +0300

    Добавил конспект первого семинара и решенное ДЗ к первому семинару

---
+++ Seminar001/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Imput a positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current ++;
+}
---
+++ homeworkSeminar001/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.
+*/
+
+/*
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if(num1 > num2)
+{
+    Console.WriteLine($"Максимальное число: {num1}");
+}
+else
+    Console.WriteLine($"Максимальное число: {num2}");
+*/
+
+/*
+Задача 4: Напишите программу, которая принимает на вход три числа 
+и выдаёт максимальное из этих чисел.
+*/
+
+/*
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int maxNum;
+if(num1 > num2)
+{
+    maxNum = num1;
+}
+else
+{
+    maxNum = num2;
+}
+if(maxNum > num3)
+{
+    Console.WriteLine($"Максимальное число: {maxNum}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число: {num3}");
+}
+*/
+
+/*
+Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+является ли число чётным (делится ли оно на два без остатка).
+*/
+
+/*
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num%2 == 0)
+{
+    Console.WriteLine("Число чётное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}
+*/
+
+/*
+Задача 8: Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+*/
+
+/*
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+while(current <= num)
+{
+    Console.Write($"{current} " );
+    current+=2;
+}
+*/
